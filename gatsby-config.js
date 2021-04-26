e6a803b6388cb378e49fb7d556e3db6e6bc97df0
Marcelo Alexandre <marcelobalexandre@gmail.com>
2021-04-26 19:13:31 -0300

    Update Gatsby to 3.3.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
     siteUrl: "https://kellynemarcelo.com",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-      },
-    },
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-google-analytics",
